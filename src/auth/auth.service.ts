fdea0d122d3aad55c6cfdac807392b50c3aafa49
IamHaydenNg <ngheiyue.dee@gmail.com>
2022-08-03 18:07:28 +0800

    Integrate argon hash and class validator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'I have signed up' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    // return the saved user
+    return user;
   }
 
   signin() {
